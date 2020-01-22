565989e005c86f2e0d0af3c8a72ae4a553b402f2
cladams0203 <cladams0203@gmail.com>
2020-01-22 13:09:51 -0500

    successful conversion to postgres

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,33 @@
 // Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
+    useNullAsDefault: true, // needed for sqlite
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    // add the following
+    // pool: {
+    //   afterCreate: (conn, done) => {
+    //     // runs after a connection is made to the sqlite engine
+    //     conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+    //   },
+    // },
+  }, 
+  staging: {
+    client: 'pastgresql',
     useNullAsDefault: true, // needed for sqlite
     connection: {
-      filename: './data/schemes.db3',
+      database: 'schemes',
+      user: 'chris',
+      password: 'password'
     },
     migrations: {
       directory: './data/migrations'
@@ -21,5 +42,5 @@ module.exports = {
         conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
       },
     },
-  }, 
+  }
 };
